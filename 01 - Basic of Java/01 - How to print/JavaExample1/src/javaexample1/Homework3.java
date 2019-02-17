02349107b986363dd5d06a566aac505e01b4c701
61070020 Tan <61070020@kmitl.ac.th>
2019-02-17 15:20:05 +0700

    update Basic of Java

---
+++ 01 - Basic of Java/01 - How to print/JavaExample1/src/javaexample1/Homework3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample1;
+
+/**
+ *
+ * @author tanknk
+ */
+public class Homework3 {
+    public static void main(String[] args){
+        System.out.println(" *********        *          **    *"); 
+        System.out.println("     *           * *         * *   *");
+        System.out.println("     *          *****        *  *  *");
+        System.out.println("     *         *     *       *   * *");
+        System.out.println("     *        *       *      *    **");
+       
+    }
+}
---
+++ 01 - Basic of Java/01 - How to print/README.md.txt
@@ -0,0 +1,14 @@
+System.out.print("Hello World!");
+System.out.print("Hello World!"); 
+
+Output
+Hello World!Hello World!
+
+
+println ปริ้นแล้วขึ้นบรรทัดใหม่ (ln = line)
+System.out.println("Hello World!");
+System.out.println("Hello World!");
+
+Output
+Hello World!
+Hello World!
